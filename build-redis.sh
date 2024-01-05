884024d295632a04316034b15d171788a3768304
Damian Kaczmarek <rush@rushbase.net>
2024-01-05 12:46:41 -0600

    Set version to 1.0.0

--- build-redis.sh
+++ build-redis.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
 docker build -t rushpl/redis-expiremember -f redis/Dockerfile .
+version=$(grep "^version" Cargo.toml | cut -d '"' -f 2)
+docker tag rushpl/redis-expiremember rushpl/redis-expiremember:${version}
